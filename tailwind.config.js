f0ab5276f3152a7772b40d01cbc0f874066f65cf
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2024-11-10 18:16:29 +0100

    setup screen size in tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,12 @@ module.exports = {
   ],
   theme: {
     extend: {
+      screens: {
+        'sm': '0px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px'
+      },
       fontFamily:{
         poppins: ["Poppins", "serif"]
       }
