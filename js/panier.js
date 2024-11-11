f5b763c57cf634b2e6855e17469a817b02b78404
majmehdi <majmehdi20@gmail.com>
2024-11-11 16:59:00 +0100

    the close function is now working

---
+++ js/panier.js
@@ -0,0 +1,10 @@
+const closeShopping = document.querySelector('#closeShopping');
+
+console.log(closeShopping)
+
+// close the shopping cart
+
+closeShopping.addEventListener("click",function(){
+    
+    shopping.classList.add("hidden");
+})
