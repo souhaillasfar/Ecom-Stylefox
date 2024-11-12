97575482a04b443bfc285d187974f49bc1716c53
majmehdi <majmehdi20@gmail.com>
2024-11-12 09:57:08 +0100

    Button with Increment and Decrement Number is now working !

--- js/panier.js
+++ js/panier.js
@@ -8,3 +8,29 @@ closeShopping.addEventListener("click",function(){
     
     shopping.classList.add("hidden");
 })
+
+const minus = document.querySelector('#minus')
+const plus = document.querySelector('#plus')
+const num = document.querySelector('#num')
+
+let x = 1;
+
+plus.addEventListener("click",()=>{
+    x++;
+    x = (x<10) ? "0" + x : x;
+    console.log("x");
+    num.innerText = x;
+});
+
+
+minus.addEventListener("click",()=>{
+
+    if(x>1){
+        x--;
+        x = (x < 10) ? "0" + x : x;
+        num.innerText = x;
+
+    }
+    console.log("x");
+   
+});
