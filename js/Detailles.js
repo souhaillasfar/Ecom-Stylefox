892f2e9aa499ac9679c6b0d8960a2c59000884df
Abderrahmane <abderrahmanahlalaly76@gmail.com>
2024-11-13 22:20:50 +0100

    ajouter fonction pour mettre à jour l'image principale

--- js/Detailles.js
+++ js/Detailles.js
@@ -11,3 +11,10 @@ function increaseQuantity() {
     const quantityInput = document.getElementById('quantity');
     quantityInput.value = parseInt(quantityInput.value) + 1;
 }
+
+// Fonction pour mettre à jour l'image principale
+function updateMainImage(thumbnailButton) {
+    const mainImage = document.getElementById('main-product-image');
+    const thumbnailImage = thumbnailButton.querySelector('img');
+    mainImage.src = thumbnailImage.src;
+}
