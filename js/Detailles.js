7b00fe3a6ab6425d7fcbfda25b944e4120057318
Abderrahmane <abderrahmanahlalaly76@gmail.com>
2024-11-13 22:18:50 +0100

    ajouter fonction pour gestionner les quantité

---
+++ js/Detailles.js
@@ -0,0 +1,13 @@
+// Fonctions pour la gestion des quantités
+function decreaseQuantity() {
+    const quantityInput = document.getElementById('quantity');
+    const currentValue = parseInt(quantityInput.value);
+    if (currentValue > 1) {
+        quantityInput.value = currentValue - 1;
+    }
+}
+
+function increaseQuantity() {
+    const quantityInput = document.getElementById('quantity');
+    quantityInput.value = parseInt(quantityInput.value) + 1;
+}
