c01fffabc4bafe7e5f15cd71574938cb0338490c
Abderrahmane <abderrahmanahlalaly76@gmail.com>
2024-11-15 16:37:12 +0100

    travail sur la foction get_product

--- js/Detailles.js
+++ js/Detailles.js
@@ -1,8 +1,14 @@
-// verification de ID
-function isexist() {
-  const url = document.URL;
-  console.log(url.split("id=")[1]);
-}
+localStorage.setItem('cart', JSON.stringify(
+  [
+    {
+      image: 'https://upload.wikimedia.org/wikipedia/en/thumb/a/a5/Grand_Theft_Auto_V.png/220px-Grand_Theft_Auto_V.png',
+      name: 'gta v',
+      size: 'XXL',
+      quantity: 4,
+      price: 50
+    }
+  ]
+));
 
 function get_product() {
   const url = document.URL;
@@ -16,19 +22,20 @@ function get_product() {
       // l'image pricipale
       const main_image = document.createElement('div');
       main_image.id = "product_images";
-      main_image.className = "space-y-4"
+      main_image.classList.add = "space-y-4"
       main_image.innerHTML = `<img src="${product.images[0]}" class="w-full h-full object-cover" id="main-product-image">`;
       main_image.appendChild(product_images);
       // les miniature
       const thumbnailImage = document.createElement('div');
       thumbnailImage.id = "miniature";
-      thumbnailImage.classList = "grid grid-cols-4 gap-4";
-      for (let i = 0; i < product.images; i++) {
+      thumbnailImage.classList.add = "grid grid-cols-4 gap-4";
+      for (let i = 0; i < product.images.lenght; i++) {
         thumbnailImage.innerHTML = `
                         <button class="product-thumbnail w-fit transition-transform duration-300 hover:scale-105"
-                            onclick="${updateMainImage(this)}">
-                            <img src="${product.images[i]}" class="w-20 h-20 object-cover rounded">
-                        </button>`;
+                          onclick="${updateMainImage(this)}">
+                          <img src="${product.images[i]}" class="w-20 h-20 object-cover rounded">
+                        </button>
+                        `;
       }
       thumbnailImage.appendChild(product_images);
       // information de produit
@@ -36,21 +43,30 @@ function get_product() {
       information.innerHTML = `
                     <h1 id="titre" class="text-3xl font-bold mb-2">${product.title}</h1>
                     <div class="flex items-center space-x-2 mb-4">
-                        <div class="text-yellow-400">★★★★☆</div>
-                        <span class="text-gray-500">${product.ra}</span>
-                        <a href="#reviews"
-                            class="text-sm text-slate-600 hover:text-slate-800 transition-colors duration-300 underline ml-2">42
-                            avis</a>
+                      <div class="text-yellow-400">★★★★☆</div>
+                      <span class="text-gray-500">${product.rate}</span>
+                      <a href="#reviews"
+                          class="text-sm text-slate-600 hover:text-slate-800 transition-colors duration-300 underline ml-2">42
+                          avis
+                      </a>
                     </div>
-      `;
+                    `;
       information.appendChild(info);
       // Prix et réduction
-
+      const prix = document.createElement('div');
+      prix.classList.add = 'flex items-baseline space-x-3';
+      prix.innerHTML = `
+                        <span id="prix" class="text-3xl font-bold text-green-600" onchange="updatePrice()">${product.price} $</span>
+                        <span id="prix_de_base" class="text-lg text-gray-500 line-through">${product.price + 25} $</span>
+                        <span id="reduction" class="text-sm font-semibold bg-slate-200 text-red-600 rounded px-2">- 25%</span>
+                      `;
+      prix.appendChild(info);
+      // option de selection
+      const option = document.getElementById('option');
 
     }
   });
-
-
+}
 
 // Fonctions pour la gestion des quantités
 function decreaseQuantity() {
