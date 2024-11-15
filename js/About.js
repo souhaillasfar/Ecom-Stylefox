1754ea51cb6193ad3edae1271a1b3521a5dfa686
FadwaElouah <fadwaelouah@gmail.com>
2024-11-15 17:28:47 +0100

    terliner partie témoignage

--- js/About.js
+++ js/About.js
@@ -172,83 +172,29 @@ fetch(window.products)
 
 .then(result=>result.json())
 .then(function(data){
-    
-    carousel.innerHTML=`
-    
-    <div class="min-w-[18rem] ml-[10px] md:ml-[1rem] mt-10 ">
-        <img src="${data[0].variants[0].images[0]}" alt=""  class="md:h-[22rem] md:w-[23rem] w-[11rem] h-[10rem] " >
-        <p class="text-[12px] font-bold  mt-[1rem] ">Thomas R, Marseille</p>
-        <p class="text-[12px] text-slate-800 mt-[10px] w-[13rem] md:min-w-[18rem]">I have purchased several items on StyleFox and I am not disappointed!</p>
-    </div>
-    
-    
-    <div class="min-w-[18rem] h-[12rem] ml-[-70px] md:ml-[2rem]  mt-10">
-        <img src="${data[0].variants[1].images[0]}"" alt="" class="md:h-[22rem] md:w-[23rem] w-[11rem] h-[10rem] ">
-        <p class="text-[12px] font-bold pt-5">Elise B., Toulouse</p>
-        <p class="text-[12px] text-slate-800 mt-[10px] w-[13rem] md:min-w-[18rem]">I love the collection offered by StyleFox. The clothes are modern, comfortable and
-            available in all sizes</p>
-    </div>
-    <div class="min-w-[18rem] ml-[-70px] md:ml-[2rem] mt-10 ">
-        <img src="${data[1].variants[0].images[0]}" alt="" class="md:h-[22rem] md:w-[23rem] w-[11rem] h-[10rem] ">
-        <p class="text-[12px] font-bold pt-5">Liam T., Customer</p>
-        <p class="text-[12px] text-slate-800 mt-[10px] w-[13rem] md:min-w-[18rem]">StyleFox has become my go-to place for all my fashion needs. The clothes are both
-            stylish and comfortable. </p>
-    </div>
-    <div class="min-w-[18rem] ml-[-70px] md:ml-[2rem] mt-10 ">
-        <img src="${data[1].variants[1].images[0]}"" alt="" class="md:h-[22rem] md:w-[23rem] w-[11rem] h-[10rem] ">
-        <p class="text-[12px] font-bold pt-5">Emma R., Customer</p>
-        <p class="text-[12px] text-slate-800 mt-[10px] w-[13rem] md:min-w-[18rem]">I've had a fantastic experience shopping with StyleFox. The quality of the clothes is
-            top-notch.</p>
-    </div>
-    <div class="min-w-[18rem] ml-[-70px] md:ml-[2rem] mt-10 ">
-        <img src="${data[2].variants[0].images[0]}" alt="" class="md:h-[22rem] md:w-[23rem] w-[11rem] h-[10rem] ">
-        <p class="text-[12px] font-bold pt-5">John D., Customer</p>
-        <p class="text-[12px] text-slate-800 mt-[10px] w-[13rem] md:min-w-[18rem]">Shopping with StyleFox was a breeze! The website was easy to navigate, and I found exactly what I was looking for.</p>
-    </div>
-    <div class="min-w-[18rem] ml-[-70px] md:ml-[2rem] mt-10 ">
-        <img src="${data[2].variants[1].images[0]}" alt="" class="md:h-[22rem] md:w-[23rem] w-[11rem] h-[10rem] ">
-        <p class="text-[12px] font-bold pt-5">Sarah M., Customer</p>
-        <p class="text-[12px] text-slate-800 mt-[10px] w-[13rem] md:min-w-[18rem]">I am absolutely in love with StyleFox! The clothing is stylish, comfortable, and the quality is outstanding</p>
-    </div>
-    <div class="min-w-[18rem] ml-[-70px] md:ml-[2rem] mt-10 ">
-        <img src="${data[3].variants[0].images[0]}" alt="" class="md:h-[22rem] md:w-[23rem] w-[11rem] h-[10rem] ">
-        <p class="text-[12px] font-bold pt-5">Olivia H., Customer</p>
-        <p class="text-[12px] text-slate-800 mt-[10px] w-[13rem] md:min-w-[18rem]">StyleFox has an amazing selection of clothing that suits all my fashion needs.</p>
-    </div>
-    <div class="min-w-[18rem] ml-[-70px] md:ml-[2rem] mt-10">
-        <img src="${data[3].variants[1].images[0]}" alt="" class="md:h-[22rem] md:w-[23rem] w-[11rem] h-[10rem] ">
-        <p class="text-[12px] font-bold pt-5">Olivia H., Customer</p>
-        <p class="text-[12px] text-slate-800 mt-[10px] w-[13rem] md:min-w-[18rem]">StyleFox has an amazing selection of clothing that suits all my fashion needs.</p>
-    </div>
-    <div class="min-w-[18rem] ml-[-70px] md:ml-[2rem] mt-10  slide">
-        <img src="${data[4].variants[0].images[0]}" alt="" class="md:h-[22rem] md:w-[23rem] w-[11rem] h-[10rem] ">
-        <p class="text-[12px] font-bold pt-5">Olivia H., Customer</p>
-        <p class="text-[12px] text-slate-800 mt-[10px] w-[13rem] md:min-w-[18rem]">StyleFox has an amazing selection of clothing that suits all my fashion needs.</p>
-    </div> 
-    <div class="min-w-[18rem] ml-[-70px] md:ml-[2rem] mt-10  slide">
-        <img src="${data[4].variants[1].images[0]}" alt="" class="md:h-[22rem] md:w-[23rem] w-[11rem] h-[10rem] ">
-        <p class="text-[12px] font-bold pt-5">Olivia H., Customer</p>
-        <p class="text-[12px] text-slate-800 mt-[10px] w-[13rem] md:min-w-[18rem]">StyleFox has an amazing selection of clothing that suits all my fashion needs.</p>
-    </div>
-    <div class="min-w-[18rem] ml-[-70px] md:ml-[2rem] mt-10 slide">
-        <img src="${data[5].variants[0].images[0]}" alt="" class="md:h-[22rem] md:w-[23rem] w-[11rem] h-[10rem] ">
-        <p class="text-[12px] font-bold pt-5">Olivia H., Customer</p>
-        <p class="text-[12px] text-slate-800 mt-[10px] w-[13rem] md:min-w-[18rem]">StyleFox has an amazing selection of clothing that suits all my fashion needs.</p>
-    </div>
-    <div class="min-w-[18rem] ml-[-70px] md:ml-[2rem] mt-10  slide">
-        <img src="${data[5].variants[1].images[0]}" alt="" class="md:h-[22rem] md:w-[23rem] w-[11rem] h-[10rem] ">
-        <p class="text-[12px] font-bold pt-5">Olivia H., Customer</p>
-        <p class="text-[12px] text-slate-800 mt-[10px] w-[13rem] md:min-w-[18rem]">StyleFox has an amazing selection of clothing that suits all my fashion needs.</p>
-    </div>
-    <div class="min-w-[18rem] ml-[-70px] md:ml-[2rem] mt-10  slide">
-        <img src="${data[6].variants[0].images[0]}"" alt="" class="md:h-[22rem] md:w-[23rem] w-[11rem] h-[10rem] ">
-        <p class="text-[12px] font-bold pt-5">Olivia H., Customer</p>
-        <p class="text-[12px] text-slate-800 mt-[10px] w-[13rem] md:min-w-[18rem]">StyleFox has an amazing selection of clothing that suits all my fashion needs.</p>
-    </div>
 
-`
+
+    data.forEach(item => {
+        let gray = 5 - parseInt(item.rate)
         
-    
+        const element = document.createElement('div')
+        element.className = 'min-w-[18rem] ml-[10px] md:ml-[1rem] mt-10'
+        element.innerHTML += `<img src="${item.variants[0].images[0]}" alt=""  class="md:h-[22rem] md:w-[23rem] w-[11rem] h-[10rem] " >`
+        element.innerHTML += `<p class="text-[12px] font-bold  mt-[1rem] ">${item.title}</p>`
+        element.innerHTML += `<p class="text-[12px] text-slate-800 mt-[10px] w-[13rem] md:min-w-[18rem]">${item.description}</p>`
+        for(let i=0; i< parseInt(item.rate); i++){
+            element.innerHTML += `<i class="fa-solid fa-star text-yellow-400"></i>`
+        }
+
+        for(let i=0; i< parseInt(gray); i++){
+            element.innerHTML += `<i class="fa-solid fa-star text-gray-400"></i>`
+        }
+
+        carousel.appendChild(element)
+
+        
+    })
+ 
 })
 
 
